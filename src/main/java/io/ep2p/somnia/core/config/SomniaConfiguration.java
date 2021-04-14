43c98fd61bd5d146d484d0a075a11d078a182546
sepehr-gh <sepehrgh01@gmail.com>
2021-04-14 11:04:49 +0430

    feat: add SomniaConfiguration

---
+++ src/main/java/io/ep2p/somnia/core/config/SomniaConfiguration.java
@@ -0,0 +1,24 @@
+package io.ep2p.somnia.core.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SomniaConfiguration {
+    private int networkSize;
+    private String databaseFingerprint;
+
+
+    private static SomniaConfiguration instance;
+
+    private SomniaConfiguration(){}
+
+    public synchronized SomniaConfiguration getInstance(){
+        if (instance == null)
+            instance = new SomniaConfiguration();
+
+        return instance;
+    }
+
+}

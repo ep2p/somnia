f702b3b659e60f82c44b051675680347b0085552
Sepehr Ghorbanpoor <sepehrgh01@gmail.com>
2021-05-08 13:42:22 +0430

    test: GenericObj

---
+++ src/test/java/io/ep2p/somnia/GenericObjTest.java
@@ -0,0 +1,23 @@
+package io.ep2p.somnia;
+
+import io.ep2p.somnia.spring.mock.SampleData;
+import io.ep2p.somnia.spring.mock.SampleSomniaEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+public class GenericObjTest {
+
+    @Test
+    public void getGenericClassType(){
+        SampleSomniaEntity sampleSomniaEntity = new SampleSomniaEntity();
+        Class<Object> genericClassType = sampleSomniaEntity.getGenericClassType(0);
+        Assertions.assertEquals(genericClassType, SampleData.class);
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                sampleSomniaEntity.getGenericClassType(1);
+            }
+        });
+    }
+}

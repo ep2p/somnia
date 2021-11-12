18e64e8dabda80ff7172ade9a346f5f31696e84d
sepehr-gh <sepehrgh01@gmail.com>
2021-11-12 20:36:56 +0330

    feat: pinging republish requester if current node is closer

--- src/main/java/io/ep2p/somnia/decentralized/protocol/handler/RepublishRequestMessageHandler.java
+++ src/main/java/io/ep2p/somnia/decentralized/protocol/handler/RepublishRequestMessageHandler.java
@@ -6,17 +6,21 @@ import io.ep2p.kademlia.node.external.ExternalNode;
 import io.ep2p.kademlia.protocol.handler.MessageHandler;
 import io.ep2p.kademlia.protocol.message.EmptyKademliaMessage;
 import io.ep2p.kademlia.protocol.message.KademliaMessage;
+import io.ep2p.kademlia.protocol.message.PingKademliaMessage;
 import io.ep2p.somnia.decentralized.SomniaConnectionInfo;
 import io.ep2p.somnia.decentralized.SomniaKeyHashGenerator;
 import io.ep2p.somnia.decentralized.protocol.message.RepublishMessage;
 import io.ep2p.somnia.model.SomniaKey;
 import io.ep2p.somnia.service.DistributionJobManager;
 import io.ep2p.somnia.util.ExternalNodeHelper;
+import lombok.extern.slf4j.Slf4j;
 import lombok.var;
 
 import java.math.BigInteger;
 import java.util.Optional;
 
+
+@Slf4j
 public class RepublishRequestMessageHandler implements MessageHandler<BigInteger, SomniaConnectionInfo> {
     private final KeyHashGenerator<BigInteger, SomniaKey> somniaKeyHashGenerator;
     private final DistributionJobManager distributionJobManager;
@@ -39,6 +43,11 @@ public class RepublishRequestMessageHandler implements MessageHandler<BigInteger
         for (ExternalNode<BigInteger, SomniaConnectionInfo> externalNode: findNodeAnswer.getNodes()) {
             if (externalNode.getId().equals(kademliaNode.getId())){
                 distributionJobManager.addJob(republishMessage.getData().getSomniaKey(), republishMessage.getData().getRequester());
+                kademliaNode.getMessageSender().sendAsyncMessage(
+                        kademliaNode,
+                        externalNode,
+                        new PingKademliaMessage<>()
+                );
                 break;
             }
 
@@ -55,7 +64,7 @@ public class RepublishRequestMessageHandler implements MessageHandler<BigInteger
                     break;
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("Failed to forward RepublishMessage to node " + externalNode.getId(), e);
             }
         }
 
